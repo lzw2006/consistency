4f945d79d94d7ca460fa22e3bce3b7ceeeccb2a2
RayLeung <lzw_20061000@126.com>
2017-09-14 16:04:55 +0800

    initial commit

---
+++ flight-service/src/main/java/com/lzw/flightservice/FlightServiceApplication.java
@@ -0,0 +1,12 @@
+package com.lzw.flightservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightServiceApplication.class, args);
+	}
+}
